845dfe9d8eb1ab855463f17b61b6879f4057fd32
username <kevinleo1235@student.ub.ac.id>
2023-07-13 18:02:29 +0700

    first

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,22 @@
 import React from "react";
+import alam from "../assets/besuki2.jpg";
 
 const Header = () => {
   return (
-    <div className="flex justify-center items-center h-screen">
+    <div
+      className="flex bg-[#F1EEA4] justify-center items-center h-screen"
+      style={{
+        backgroundImage: `url(${alam})`,
+       
+        backgroundRepeat:"no-repeat",
+
+      }}
+    >
       <div className="text-center">
-        <h1 className="text-6xl font-bold text">Pasar Besuki</h1>
-        <p className="font-bold text-black font-[Montserrat] ">Memperkuat UMKM dan Pesona Desa Besuki Tulungagung</p>
+        <h1 className="text-6xl font-bold text ">Pasar Besuki</h1>
+        <p className="font-bold text-white font-[Montserrat]">
+          Memperkuat UMKM dan Pesona Desa Besuki Tulungagung
+        </p>
       </div>
     </div>
   );
